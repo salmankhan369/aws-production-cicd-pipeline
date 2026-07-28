45aa9c1290f0c701957ad4f496616950a29e265e
Salman Khan <Salman0507sam@gmail.com>
2026-07-28 22:52:44 +0530

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,5 +12,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Yahan aapke main infrastructure resources rahenge (EKS/EC2/Security Groups etc.)
-# S3 aur DynamoDB resources bilkul remove kar diye hain.
